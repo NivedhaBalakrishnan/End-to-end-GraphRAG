4133c4b8e2804a072f8472b975b3ff243a257e55
Nivedha Balakrishnan <nivedhabalakrishnan@MacBook-Pro.local>
2024-05-05 00:32:20 -0700

    embedding added

--- graphQA.py
+++ graphQA.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from graphRAG import GraphRAG
+from deprecated.graphRAG import GraphRAG
 from RAG import RAG
 import os
 
--- graphRAG.py
+++ graphRAG.py
@@ -1,111 +1,155 @@
-import os
-from helper import Helper
-# Common data processing
-import json
-import textwrap
-import pandas as pd
-
-from dotenv import load_dotenv 
-
-# Langchain
-from langchain_community.graphs import Neo4jGraph
-from langchain_community.vectorstores import Neo4jVector
-from langchain_openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain_openai import ChatOpenAI
-
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings
-
 from neo4j import GraphDatabase
-import openai
+from langchain.chains import GraphCypherQAChain
+from langchain_community.graphs import Neo4jGraph
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+import os
+import textwrap
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Neo4jVector
+from langchain.chains import RetrievalQAWithSourcesChain
 
-import time
 
 
 
-class GraphRAG():
+class NLQ():
     def __init__(self) -> None:
+        load_dotenv()
+        NEO4J_URI = os.environ.get("NEO4J_URI")
+        NEO4J_USERNAME = os.environ.get("NEO4J_USERNAME")
+        NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD")
+        self.graph = Neo4jGraph(url=NEO4J_URI, username=NEO4J_USERNAME, password=NEO4J_PASSWORD)
+        API_KEY = os.getenv('ANTHROPIC_API_KEY') # Authentication
+        self.chain = GraphCypherQAChain.from_llm(
+            ChatAnthropic(model="claude-2.1", anthropic_api_key=API_KEY), graph=self.graph, verbose=True)
+    
+    def get_response(self, question):
+        response = self.chain.run("Recommended food for hypertension?")
+        return response
 
-        load_dotenv('.env', override=True)
-        hlp = Helper()
-        hlp.clear_log_file()
-        self.log = hlp.get_logger()
+
+class GraphEmbeddings():
+    def __init__(self) -> None:
+        os.environ["TOKENIZERS_PARALLELISM"] = "false"
+        load_dotenv()
+        self.NEO4J_URI = os.environ.get("NEO4J_URI")
+        self.NEO4J_USERNAME = os.environ.get("NEO4J_USERNAME")
+        self.NEO4J_PASSWORD = os.environ.get("NEO4J_PASSWORD")
+        self.driver = GraphDatabase.driver(self.NEO4J_URI, auth=(self.NEO4J_USERNAME, self.NEO4J_PASSWORD))
+        self.embedding_function = HuggingFaceEmbeddings()
+        self.index_name = "nodeEmbeddings"
+        self.embedding_dimensions = 768
         
-        self.NEO4J_URI = os.getenv('NEO4J_URI')
-        self.NEO4J_USERNAME = os.getenv('NEO4J_USERNAME')
-        self.NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD')
-        self.NEO4J_DATABASE = os.getenv('NEO4J_DATABASE') or 'neo4j'
 
-        self.OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-        self.OPENAI_ENDPOINT = os.getenv('OPENAI_BASE_URL') + '/embeddings'
 
-        self.VECTOR_INDEX_NAME = 'article_chunk'
-        self.VECTOR_NODE_LABEL = 'Chunk'
-        self.VECTOR_SOURCE_PROPERTY = 'text'
-        self.VECTOR_EMBEDDING_PROPERTY = 'textEmbedding'
+    def retrieve_nodes_and_relationships(self, tx, node_id):
+        query = """
+        MATCH (n)-[r]->(m)
+        WHERE n.id = $node_id
+        RETURN n.id AS node1, type(r) AS relationship, m.id AS node2, '->' AS direction
+        UNION
+        MATCH (n)<-[r]-(m)
+        WHERE n.id = $node_id
+        RETURN n.id AS node1, type(r) AS relationship, m.id AS node2, '<-' AS direction
+        """
+        result = tx.run(query, node_id=node_id)
+        return [(record["node1"], record["relationship"], record["node2"], record["direction"]) for record in result]
 
-  
+    def generate_embeddings(self):
+        with self.driver.session() as session:
+            nodes = session.run("MATCH (n) RETURN n as node").data()
+            for node in nodes:
+                node_id = node['node']["id"]
+                data = session.read_transaction(self.retrieve_nodes_and_relationships, node_id)
+                paragraph = ""
+                for node1, relationship, node2, direction in data:
+                    sentence = f"{node1} {direction} {relationship} {direction} {node2}."
+                    paragraph += sentence + " "
+                embedding = self.embedding_function.embed_query(paragraph.strip())
+                session.run(
+                    "MATCH (n {id: $node_id}) SET n.embedding = $embedding, n.info = $paragraph",
+                    node_id=node_id,
+                    embedding=list(embedding),
+                    paragraph=paragraph.strip()
+                )
+        
+    
+    def create_vector_index(self):
+        with self.driver.session() as session:
+            session.run("""
+            CREATE VECTOR INDEX nodeEmbeddings IF NOT EXISTS
+            FOR (node:id) ON (node.embedding)
+            OPTIONS {
+                indexConfig: {
+                    `vector.dimensions`: 768,
+                    `vector.similarity_function`: 'cosine'
+                }
+            }
+        """)
+
+    def close(self):
+        self.driver.close()
+
     
     def get_retriever(self):
         try: 
             neo4j_vector_store = Neo4jVector.from_existing_graph(
-                embedding = OpenAIEmbeddings(api_key=self.OPENAI_API_KEY),
+                embedding = self.embedding_function,
                 url=self.NEO4J_URI,
                 username=self.NEO4J_USERNAME,
                 password=self.NEO4J_PASSWORD,
-                database=self.NEO4J_DATABASE,
-                index_name=self.VECTOR_INDEX_NAME,
-                node_label=self.VECTOR_NODE_LABEL,
-                embedding_node_property=self.VECTOR_EMBEDDING_PROPERTY,
-                text_node_properties=[self.VECTOR_SOURCE_PROPERTY]
+                database="neo4j",
+                index_name="nodeEmbeddings",
+                node_label="id",
+                embedding_node_property="embedding",
+                text_node_properties=["id","info"]
             )
             retriever = neo4j_vector_store.as_retriever()
-            self.log.info('Retriever created successfully')
+            print('Retriever created successfully')
             return retriever
         except Exception as e:
-            self.log.error(f'Error creating retriever: {e}')
+            print(f'Error creating retriever: {e}')
             raise e
-
-
-
+    
     def get_chain(self):
         try:
             retriever = self.get_retriever()
             
             if retriever:
+                API_KEY = os.environ.get('ANTHROPIC_API_KEY')
                 chain = RetrievalQAWithSourcesChain.from_chain_type(
-                    ChatOpenAI(temperature=0.5),
+                    ChatAnthropic(model="claude-2.1", anthropic_api_key=API_KEY),
                     chain_type="stuff",
                     retriever=retriever, return_source_documents=True)
 
-                self.log.info('Chain created successfully')
+                print('Chain created successfully')
                 return chain
             else:
-                self.log.error('Retriever not created')
+                print('Retriever not created')
                 return None
         except Exception as e:
-            self.log.error(f'Error creating chain: {e}')
+            print(f'Error creating chain: {e}')
             raise e
     
-
     def get_response(self, question):
         chain = self.get_chain()
         try:
             """Pretty print the chain's response to a question"""
-            start_time = time.time()
-            response = chain.invoke({"question": question})
-
-            end_time = time.time()  # Store the end time
-            elapsed_time = end_time - start_time
-
-            sources = response.get('source_documents', '')
-            source_content = [doc.page_content for doc in sources]
-            print(f"Source: {source_content}")
-            return textwrap.fill(response['answer'], 60), source_content, elapsed_time
+            response = chain.invoke({"question": question}, verbose=True)
+            print(response)
+            # sources = response.get('source_documents', '')
+            # source_content = [doc.page_content for doc in sources]
+            # return textwrap.fill(response['answer'], 60)
         except Exception as e:
-            self.log.error(f'Error pretty printing chain: {e}')
+            print(f'Error pretty printing chain: {e}')
             raise e
 
-
-    
+        
+# Usage example
+graph_embeddings = GraphEmbeddings()
+# graph_embeddings.generate_embeddings()
+# graph_embeddings.create_vector_index()
+# graph_embeddings.close()
+question = "Diet for rheumatoid pain."
+response = graph_embeddings.get_response(question)
+# print(response)
