5155d3b9913f1b7133bdbb9b42e39c790d3e1604
Nivedha Balakrishnan <nivedhabalakrishnan@MacBook-Pro.local>
2024-05-06 15:44:55 -0700

    for eval

--- RAG.py
+++ RAG.py
@@ -2,7 +2,10 @@ from vectorize import VectorDB
 import anthropic
 import os
 import logging
-
+import pandas as pd
+from eval import Evaluation
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 # create log file
@@ -50,7 +53,109 @@ class RAG():
         logging.info(text)
 
         response = self.model_prediction(question, text)
-        return response       
+        return response
+    
+    def get_response_eval(self,question):
+        vector_db = VectorDB()
+        docs = vector_db.get_db_retriever(question, k=3)
+        logging.info(docs)
+        text = [doc.page_content for doc in docs]
+        logging.info(text)
+
+        response = self.model_prediction(question, text)
+        print(response)
+        return question, text, response
+    
+    def get_evaluated(self, question, source, answer):
+
+            evaluation = Evaluation()
+            # embeddings_model = OpenAIEmbeddings(api_key=os.environ.get('OPENAI_API_KEY'))
+            embeddings_model = HuggingFaceEmbeddings()
+            # similarity_score = self.evaluation.evaluate_similarity(
+            #     embeddings_model,
+            #     answer,
+            #     source
+            # )
+            groundedness_score = evaluation.groundedness(question, source, answer)
+            context_relevancy_score = evaluation.context_relevancy(question, source, answer)
+            score, reason = evaluation.evaluate(question, source, answer)
+            # coherence_score, coherence_reason = evaluation.evaluate_coherence(question, answer)
+            faithfulness_score, faithfulness_reason = evaluation.evaluate_faithfulness(question, answer, source)
+            # #contextual_precision_score, contextual_precision_reason = evaluation.evaluate_contextual_precision(question, answer, source)
+            # #contextual_recall_score, contextual_recall_reason = evaluation.evaluate_contextual_recall(question, answer, source)
+            hallucination_score, hallucination_reason = evaluation.evaluate_hallucination(question, answer, source)
+            # toxicity_score, toxicity_reason = evaluation.evaluate_toxicity(question, answer)
+            # bias_score, bias_reason = evaluation.evaluate_bias(question, answer)
+            # #ragas_score = evaluation.evaluate_ragas(question, answer, source)
+
+            # return (similarity_score, score, reason, coherence_score, coherence_reason, faithfulness_score,
+            #         faithfulness_reason, hallucination_score, hallucination_reason,
+            #         toxicity_score, toxicity_reason, bias_score, bias_reason)
+    
+
+    def save_to_csv(self, question, source, answer, scores):
+
+        data = {
+            'Question': question,
+            'Source': source,
+            'Answer': answer,
+            'Similarity Score': scores[0],
+            'Relevancy Score': scores[1],
+            'Reason': scores[2],
+            'Coherence Score': scores[3],
+            'Coherence Reason': scores[4],
+            'Faithfulness Score': scores[5],
+            'Faithfulness Reason': scores[6],
+            'Hallucination Score': scores[7],
+            'Hallucination Reason': scores[8],
+            'Toxicity Score': scores[9],
+            'Toxicity Reason': scores[10],
+            'Bias Score': scores[11],
+            'Bias Reason': scores[12]
+        }
+
+        df = pd.DataFrame([data])
+
+        file_exists = os.path.isfile('er_metrics.csv')
+
+        if file_exists:
+            df.to_csv('er_metrics.csv', mode='a', header=False, index=False)
+        else:
+            df.to_csv('er_metrics.csv', index=False)
+
+        print("Scores saved successfully.")
+
+
+
+
+if __name__ == '__main__':
+    rag = RAG()
+    questions = [
+    "What are some of the key foods and strategies focused on in an anti-inflammatory diet?",
+    "Why is reducing consumption of processed and red meat recommended for an anti-inflammatory eating approach?",
+    "What role does achieving a healthy weight play in reducing inflammation in the body?",
+    "How can probiotics and prebiotics help improve gut health and reduce inflammation?",
+    "What are the recommended omega-6 to omega-3 ratios to aim for in an anti-inflammatory diet?",
+    "Why are whole foods preferred over isolating specific nutrients in an anti-inflammatory eating plan?",
+    "What are some of the health conditions that research indicates may benefit from following an anti-inflammatory diet?",
+    "How can herbs and spices like turmeric, ginger and rosemary help reduce inflammation when included regularly in the diet?",
+    "What are the potential benefits of plant-based proteins like legumes, nuts and seeds compared to animal proteins in terms of inflammation?",
+    "Why is it important to focus on foods to include more of rather than just foods to restrict or avoid in an anti-inflammatory eating approach?"
+    ]
+    total_elapsed_time = 0
+    question_count = 0
+    for question in questions:
+        answer, source, elapsed_time = rag.get_response_eval(question)
+        print(f"A: {answer}")
+        scores = rag.get_evaluated(question, source, answer)
+        rag.save_to_csv(question, source, answer, scores)
+
+        total_elapsed_time += elapsed_time
+        question_count += 1
+
+    average_time = total_elapsed_time / question_count
+    print(f'Average time per question: {average_time} seconds')
+
 
 
 # if __name__ == "__main__":
---
+++ eval.py
@@ -0,0 +1,149 @@
+from deepeval import evaluate
+from deepeval.metrics import AnswerRelevancyMetric,GEval, FaithfulnessMetric, ContextualPrecisionMetric, ContextualRecallMetric, HallucinationMetric, ToxicityMetric, BiasMetric
+from deepeval.test_case import LLMTestCase, LLMTestCaseParams
+from datetime import datetime
+from scipy.spatial.distance import cosine
+import os
+
+config = {
+    "embedding_model": "bert-base-uncased",
+    "model": {
+        "type": "anthropic",
+        "api_key": os.environ.get('ANTHROPIC_API_KEY'),
+        "model_name": "claude-2.1",
+    }
+}
+
+class Evaluation:
+    def __init__(self, model_name="gpt-3.5"):
+        self.model_name = model_name
+        
+    def evaluate(self, question,source,answer):
+        answer_relevancy_metric = AnswerRelevancyMetric(threshold=0.7)
+        retrieval_context = [source] if source is not None else None
+        test_case = LLMTestCase(
+            input= question,
+            actual_output= answer,
+            retrieval_context= retrieval_context
+        )
+        print("Answer Relevancy Metric Score and Reason")
+        evaluate([test_case], [answer_relevancy_metric])
+        return answer_relevancy_metric.score,answer_relevancy_metric.reason
+    
+
+    def cosine_similarity(self, vec1, vec2):
+        if all(v == 0 for v in vec1) or all(v == 0 for v in vec2):
+            return 0.0
+        return 1 - cosine(vec1, vec2)
+
+
+    def evaluate_similarity(self, embeddings_model, answer, source):
+        vec1 = embeddings_model.embed_query(answer)
+        vec2 = embeddings_model.embed_query(source)
+        if not vec1 or not vec2:
+            raise ValueError("One of the embedding vectors is empty.")
+        return self.cosine_similarity(vec1, vec2)
+    
+
+    def evaluate_coherence(self, input_text, actual_output):
+        # Define coherence evaluation logic here
+        coherence_metric = GEval(
+            name="Coherence",
+            criteria="Determine if the actual output is coherent with the input.",
+            evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.ACTUAL_OUTPUT],
+        )
+        test_case = LLMTestCase(
+            input=input_text,
+            actual_output=actual_output
+        )
+        coherence_metric.measure(test_case)
+        evaluate([test_case], [coherence_metric])
+        return coherence_metric.score, coherence_metric.reason
+    
+    def groundedness(self, input_text, source, actual_output):
+        coherence_metric = GEval(
+            name="Groundedness",
+            criteria="Determine if the actual output is grounded to the context.",
+            evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.RETRIEVAL_CONTEXT, LLMTestCaseParams.ACTUAL_OUTPUT],
+        )
+        test_case = LLMTestCase(
+            input=input_text,
+            retrieval_context=source,
+            actual_output=actual_output
+            
+        )
+        coherence_metric.measure(test_case)
+        evaluate([test_case], [coherence_metric])
+        return coherence_metric.score, coherence_metric.reason
+    
+    def context_relevancy(self, input_text, source, actual_output):
+        coherence_metric = GEval(
+            name="Context Relevance",
+            criteria="Determine if the actual output is relevant to the context.",
+            evaluation_params=[LLMTestCaseParams.INPUT, LLMTestCaseParams.RETRIEVAL_CONTEXT],
+        )
+        test_case = LLMTestCase(
+            input=input_text,
+            actual_output=actual_output,
+            retrieval_context=source
+        )
+        coherence_metric.measure(test_case)
+        evaluate([test_case], [coherence_metric])
+        return coherence_metric.score, coherence_metric.reason
+    
+
+    def evaluate_faithfulness(self, input_text, actual_output, retrieval_context):
+        metric = FaithfulnessMetric(
+            threshold=0.7,
+            model="gpt-4",  
+            include_reason=True
+        )
+        retrieval_context = [retrieval_context] if retrieval_context is not None else None
+        test_case = LLMTestCase(
+            input=input_text,
+            actual_output=actual_output,
+            retrieval_context=retrieval_context
+        )
+        metric.measure(test_case)
+        evaluate([test_case], [metric])
+        return metric.score, metric.reason
+
+
+    def evaluate_hallucination(self, input_text, actual_output, context):
+        retrieval_context = [context] if context is not None else None
+        metric = HallucinationMetric(threshold=0.5)
+        test_case = LLMTestCase(
+            input=input_text,
+            actual_output=actual_output,
+            context= retrieval_context
+        )
+        metric.measure(test_case)
+        evaluate([test_case], [metric])
+        return metric.score, metric.reason
+
+    def evaluate_toxicity(self, input_text, actual_output):
+        metric = ToxicityMetric(threshold=0.5)
+        test_case = LLMTestCase(
+            input=input_text,
+            actual_output=actual_output
+        )
+        metric.measure(test_case)
+        evaluate([test_case], [metric])
+        return metric.score, metric.reason
+
+    def evaluate_bias(self, input_text, actual_output):
+        metric = BiasMetric(threshold=0.5)
+        test_case = LLMTestCase(
+            input=input_text,
+            actual_output=actual_output
+        )
+        metric.measure(test_case)
+        evaluate([test_case], [metric])
+        return metric.score, metric.reason
+
+
+
+
+
+
+
